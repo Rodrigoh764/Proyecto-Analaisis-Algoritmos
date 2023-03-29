8f3ca2c0c4ef3d3a0abdca9435d9b981f9302174
Rodrigo <86635430+Rodrigoh764@users.noreply.github.com>
2023-03-28 20:56:38 -0600

    se agregaron funciones

--- Practica1.py
+++ Practica1.py
@@ -1,60 +1,60 @@
-def agregaAlumno():
-    print("Agregar alumnos")
+class Alumno:
+    def __init__(self,serie, nombre,nota1,nota2,nota3):
+        self.serie = serie
+        self.nombre = nombre
+        self.nota1 = nota1
+        self.nota2 = nota2
+        self.nota3 = nota3
+    def __str__(self):
+        return f'{self.nombre} Notas:[{self.nota1}-{self.nota2}-{self.nota3}]'
+    
+class Escuela:
+    
+    def __init__(self):
+        self.listaEstudiantes = []
 
-def calculaPromedio():
-    print("Promedios")
-
-def alumnosAprobados():
-    print("Alumnos Aprobados")
-
-def alumnosReprobados():
-    print("Alumnos reporbados")
-
-def calificacionMaxParcial():
-    print("calificacion maxima del parcial")
-
-def calificacionMinParcial():
-    print("calificacion minima del parcial")
-
-def promedioMax():
-    print("Promedio maximo")
-
-def promedioMin():
-    print("promedio minimo")
+    def agregaAlumno(self, nombre):
+        self.listaEstudiantes.append(nombre)
 
 
-def menu():
-   
+    
+    def mostrarAlumnos(self):
+        for p in self.listaEstudiantes:
+            print(p)
+
+
+
+def menu(escuela):
+    cont = len(escuela.listaEstudiantes)
     while True:
         print('\nInstitucion Escolar ' '\n')
         print('[1]: Agregar alumnos')
-        print('[2]: Promedio de todos los alumnos')
-        print('[3]: Alumnos aprobados')
-        print('[4]: Alumnos reprobados')
-        print('[5]: Calificacion maxima por parcial')
-        print('[6]: Calificacion minima por parcial')
-        print('[7]: Promedio maximo')
-        print('[8]: Promedio minimo')
-        print('[9]: Salir del programa')
+        print("[2]: Mostrar los alumnos registrados")
+        print('[3]: Promedio de todos los alumnos')
+        print('[4]: Alumnos aprobados')
+        print('[5]: Alumnos reprobados')
+        print('[6]: Calificacion maxima por parcial')
+        print('[7]: Calificacion minima por parcial')
+        print('[8]: Promedio maximo')
+        print('[9]: Promedio minimo')
+        print('[10]: Salir del programa')
         op = input('Opcion -> ')
         if op == '1':
-            agregaAlumno()
+            nombre = input("Introduce el nombre del alumno:\n")
+            nota1 = input("califcacion de la nota 1:\n")
+            nota2 = input("califcacion de la nota 2:\n")
+            nota3 = input("califcacion de la nota 3:\n")
+            escuela.agregaAlumno(Alumno(cont+1, nombre,nota1,nota2,nota3))
+            
+                
+            cont+=1
         elif op == '2':
-            calculaPromedio()
-        elif op == '3':
-            alumnosAprobados()
-        elif op == '4':
-            alumnosReprobados()
-        elif op == '5':
-            calificacionMaxParcial()
-        elif op == '6':
-            calificacionMinParcial()
-        elif op == '7':
-            promedioMax()
-        elif op == '8':
-            promedioMin()
-        elif op == '9':
+            print("\nLista de alumnos inscritos\n")
+            escuela.mostrarAlumnos()
+        elif op == '10':      
             break
 
 
-menu()
+escuela = Escuela()
+
+menu(escuela)
