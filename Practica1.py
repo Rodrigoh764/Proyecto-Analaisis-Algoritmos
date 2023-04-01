fbbdb45f4f4f5676cd1d76b1f688687d8c846c13
Rodrigo <86635430+Rodrigoh764@users.noreply.github.com>
2023-04-01 01:31:19 -0600

    se cambio el diseño a matrices

--- Practica1.py
+++ Practica1.py
@@ -1,60 +1,30 @@
-class Alumno:
-    def __init__(self,serie, nombre,nota1,nota2,nota3):
-        self.serie = serie
-        self.nombre = nombre
-        self.nota1 = nota1
-        self.nota2 = nota2
-        self.nota3 = nota3
-    def __str__(self):
-        return f'{self.nombre} Notas:[{self.nota1}-{self.nota2}-{self.nota3}]'
+numAlumnos = int(input("cuantos alumnos: ")) 
+materia = 3 
+matrix = [] 
+for i in range(numAlumnos):           
+    a =[] 
+    for j in range(materia+1):
+        if j ==0:
+                print("Nombre: ")      
+                a.append(input())
+        else:
+            print("Nota:",j)    
+            a.append(int(input())) 
+    matrix.append(a) 
+
+
+lista = []
+for i in range(numAlumnos):
+    total=0 
+    for j in range(materia+1):
+        if j > 0: 
+            total = total + matrix[i][j]
+    print("-->",total) 
+    #lista.append(total)
+#print(lista)
+        
+
     
-class Escuela:
     
-    def __init__(self):
-        self.listaEstudiantes = []
-
-    def agregaAlumno(self, nombre):
-        self.listaEstudiantes.append(nombre)
 
 
-    
-    def mostrarAlumnos(self):
-        for p in self.listaEstudiantes:
-            print(p)
-
-
-
-def menu(escuela):
-    cont = len(escuela.listaEstudiantes)
-    while True:
-        print('\nInstitucion Escolar ' '\n')
-        print('[1]: Agregar alumnos')
-        print("[2]: Mostrar los alumnos registrados")
-        print('[3]: Promedio de todos los alumnos')
-        print('[4]: Alumnos aprobados')
-        print('[5]: Alumnos reprobados')
-        print('[6]: Calificacion maxima por parcial')
-        print('[7]: Calificacion minima por parcial')
-        print('[8]: Promedio maximo')
-        print('[9]: Promedio minimo')
-        print('[10]: Salir del programa')
-        op = input('Opcion -> ')
-        if op == '1':
-            nombre = input("Introduce el nombre del alumno:\n")
-            nota1 = input("califcacion de la nota 1:\n")
-            nota2 = input("califcacion de la nota 2:\n")
-            nota3 = input("califcacion de la nota 3:\n")
-            escuela.agregaAlumno(Alumno(cont+1, nombre,nota1,nota2,nota3))
-            
-                
-            cont+=1
-        elif op == '2':
-            print("\nLista de alumnos inscritos\n")
-            escuela.mostrarAlumnos()
-        elif op == '10':      
-            break
-
-
-escuela = Escuela()
-
-menu(escuela)
