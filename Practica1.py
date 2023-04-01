318576d0fa97897061d075a8ba4e3412f2d21bbc
Rodrigo <86635430+Rodrigoh764@users.noreply.github.com>
2023-04-01 01:32:51 -0600

    se modifico la lista de nombres

--- Practica1.py
+++ Practica1.py
@@ -1,30 +1,86 @@
-numAlumnos = int(input("cuantos alumnos: ")) 
-materia = 3 
-matrix = [] 
-for i in range(numAlumnos):           
-    a =[] 
-    for j in range(materia+1):
-        if j ==0:
-                print("Nombre: ")      
-                a.append(input())
-        else:
-            print("Nota:",j)    
-            a.append(int(input())) 
-    matrix.append(a) 
+def llenado(numAlumnos):
+    matrix = []
+    for i in range(numAlumnos):           
+        a =[] 
+        for j in range(4):
+            if j ==0:
+                    print("Nombre: ")      
+                    a.append(input())
+            else:
+                print("Nota:",j)    
+                a.append(int(input())) 
+        matrix.append(a) 
+    return  matrix
 
 
-lista = []
-for i in range(numAlumnos):
-    total=0 
-    for j in range(materia+1):
-        if j > 0: 
-            total = total + matrix[i][j]
-    print("-->",total) 
-    #lista.append(total)
-#print(lista)
+def mostrarAlumnos(lista,numAlumnos):
+    nombreAlumnos = []
+    for i in range(numAlumnos):
+        for j in range(4): 
+            print(lista[i][j], end = " ") 
+        print()
+    j=0
+    for i in range(numAlumnos):
+        nombreAlumnos.append(lista[i][j])
+
+    return nombreAlumnos
+        
+ 
+    
+ 
         
 
-    
-    
+
+def promedio(lista,numAlumnos):
+    listaPromedio = []
+    for i in range(numAlumnos):
+        total=0 
+        for j in range(4):
+            if j > 0: 
+                total = total + lista[i][j]
+                promedio = total/3
+        listaPromedio.append(promedio)
+    for i in listaPromedio:
+        print(i)
+    return listaPromedio
+
+def alumnosAprobados(promedio,nombres):
+    #for a in promedio:
+    #    if a >= 7:
+     #       print("Aprobado",a)
+      #  contador += 1
+    print(nombres[0],promedio[0])
+
+
+
+def menu():
+    while True:
+        print('\nInstitucion Escolar ' '\n')
+        print('[1]: Agregar alumnos')
+        print("[2]: Mostrar los alumnos registrados")
+        print('[3]: Promedio de todos los alumnos')
+        print('[4]: Alumnos aprobados')
+        print('[5]: Alumnos reprobados')
+        print('[6]: Calificacion maxima por parcial')
+        print('[7]: Calificacion minima por parcial')
+        print('[8]: Promedio maximo')
+        print('[9]: Promedio minimo')
+        print('[10]: Salir del programa')
+        op = input('Opcion -> ')
+        if op == '1':
+            numAlumnos = int(input("Cuantos alumnos desea registrar: "))
+            lista = llenado(numAlumnos)
+        elif op == '2':
+            nombres = mostrarAlumnos(lista,numAlumnos)
+        elif op == '3':
+            promedioGeneral = promedio(lista,numAlumnos)
+        elif op == '4':
+            alumnosAprobados(promedioGeneral,nombres)
+        elif op == '10':
+            break
+             
+
+menu()
+
 
 
